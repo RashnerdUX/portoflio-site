44d5a1fc2a8b8ce298c1d662c48fef93c2fa0a63
Rashnerd <akhigbek6@gmail.com>
2025-10-25 18:44:26 +0100

    feat: enhance VoteModal component with improved styling, close button functionality, and layout adjustments

--- my-world/app/routes/mcu/rateMovie.tsx
+++ my-world/app/routes/mcu/rateMovie.tsx
@@ -4,6 +4,7 @@ import { Loading } from '~/components/loadingIcon';
 import supabase from '~/superbaseclient';
 import { Form } from 'react-router';
 import { useRouteLoaderData, useParams } from "react-router";
+import { X } from 'lucide-react';
 
 export function meta({ params }: Route.MetaArgs) {
   return [
@@ -34,14 +35,33 @@ export const VoteModal = ({
 
   const movie_data = data.movieData;
 
+  const closeModal = () => {
+    console.log("Closing the Modal")
+  };
+
   return (
-    <div className='h-full w-[300px]  bg-amber-400'>
-      <h1>Vote for {movie_data.title}</h1>
-      <Form method='post'>
-        <label htmlFor="vote">Your Vote:</label>
-        <input type="text" name="vote" placeholder="Your Vote" />
-        <button type="submit">Submit Vote</button>
-      </Form>
+    <div className='fixed inset-0 z-50 bg-black/80 backdrop-blur-sm flex items-center justify-center'>
+      <div className='relative w-full max-w-md mx-auto bg-background rounded-2xl border border-primary/20 shadow-2xl shadow-primary/20 overflow-hidden'>
+
+        {/* Background radial */}
+        <div className="absolute top-0 left-0 w-full h-full bg-no-repeat bg-center" style={{ backgroundImage: "radial-gradient(circle at center, rgba(102, 15, 189, 0.2) 0%, rgba(102, 15, 189, 0) 70%)" }}></div>
+
+        {/* Close button */}
+        <button className="absolute top-4 right-4 z-50 text-foreground/80 hover:text-foreground transition-colors" onClick={closeModal}>
+          <X className='size-5 text-foreground font-bold' />
+        </button>
+
+        {/* Main content */}
+        <div className='relative p-6 sm:p-8 max-h-[90vh] overflow-y-auto'>
+          <div className="text-center mb-6">
+            <h2 className="text-white text-2xl sm:text-3xl font-bold leading-tight tracking-tight">Rate the Movie</h2>
+            <p className="text-primary text-xl font-medium">{movie_data.title}</p>
+          </div>
+          <Form method='post'>
+            
+          </Form>
+        </div>
+      </div>
     </div>
   )
 }
