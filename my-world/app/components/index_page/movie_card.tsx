10e84303cc30d4b556e99dbe4ebfc578f73aba00
Rashnerd <akhigbek6@gmail.com>
2025-10-30 15:39:43 +0100

    feat: enhance MovieCard component with routing and add unique IDs and slugs to movie data

--- my-world/app/components/index_page/movie_card.tsx
+++ my-world/app/components/index_page/movie_card.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import { FaGem } from "react-icons/fa";
+import { Link } from 'react-router';
 
 interface MovieCardProps {
+    id: number;
     index: number;
     title : string;
     releaseYear: number;
@@ -11,28 +13,31 @@ interface MovieCardProps {
     tvaRating: string;
     posterUrl: string;
     aggregratedRating: number;
+    slug: string;
 }
 
-export const MovieCard = ({ index, title, releaseYear, phase, synopsis, sagaRelevance, tvaRating, posterUrl, aggregratedRating }: MovieCardProps) => {
+export const MovieCard = ({ id, index, title, releaseYear, phase, synopsis, sagaRelevance, tvaRating, posterUrl, aggregratedRating, slug }: MovieCardProps) => {
   return (
-    <div className='flex flex-col lg:flex-row bg-card p-4 gap-6 rounded-xl border border-primary/20 dark:hover:border-primary transition-all dark:hover:shadow-[0_0_20px_rgba(102,15,189,0.5)] duration-300 transform hover:-translate-y-1'>
-      {/* The Image */}
-      <img src={posterUrl} alt={`${title} poster`} className='w-40 h-48 object-cover rounded-lg flex-shrink-0'/>
-      {/* Main content */}
-      <div className='flex flex-col'>
-        <div className='mb-2'>
-            <h3 className='text-2xl font-bold text-secondary-foreground'><span className="text-primary mr-2">{index + 1}.</span>{title}</h3>
-            <p className='text-secondary-foreground/50 mt-1'>{releaseYear} • Phase {phase} • {sagaRelevance}</p>
-            <p className='text-secondary-foreground/70 font-medium text-sm text-wrap mt-4'>{synopsis}</p>
-        </div>
-        <div className="mt-auto pt-4 flex items-center justify-between">
-          <span className="text-2xl font-bold text-yellow-400 dark:text-yellow-300">
-            <FaGem className='inline-block mr-2 mb-1' />
-            {aggregratedRating}</span>
-          <span className="text-xs font-bold py-1 px-3 rounded-full bg-green-500/20 text-green-400 border border-green-500/30">{tvaRating}</span>
+    <Link to={`/mcu-index/detail/${id}/${slug}`} className='no-underline'>
+      <div className='flex flex-col lg:flex-row bg-card p-4 gap-6 rounded-xl border border-primary/20 dark:hover:border-primary transition-all dark:hover:shadow-[0_0_20px_rgba(102,15,189,0.5)] duration-300 transform hover:-translate-y-1'>
+        {/* The Image */}
+        <img src={posterUrl} alt={`${title} poster`} className='w-40 h-48 object-cover rounded-lg flex-shrink-0'/>
+        {/* Main content */}
+        <div className='flex flex-col'>
+          <div className='mb-2'>
+              <h3 className='text-2xl font-bold text-secondary-foreground'><span className="text-primary mr-2">{index + 1}.</span>{title}</h3>
+              <p className='text-secondary-foreground/50 mt-1'>{releaseYear} • Phase {phase} • {sagaRelevance}</p>
+              <p className='text-secondary-foreground/70 font-medium text-sm text-wrap mt-4'>{synopsis}</p>
+          </div>
+          <div className="mt-auto pt-4 flex items-center justify-between">
+            <span className="text-2xl font-bold text-yellow-400 dark:text-yellow-300">
+              <FaGem className='inline-block mr-2 mb-1' />
+              {aggregratedRating}</span>
+            <span className="text-xs font-bold py-1 px-3 rounded-full bg-green-500/20 text-green-400 border border-green-500/30">{tvaRating}</span>
+          </div>
         </div>
       </div>
-    </div>
+    </Link>
   )
 }
 
--- my-world/app/routes.ts
+++ my-world/app/routes.ts
@@ -3,7 +3,7 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 export default [
     index("routes/home.tsx"),
     route("/mcu-index", "routes/mcu/index.tsx", {id: "MCUMovieIndex"}),
-    route("/detail/:movieId/:movieName", "routes/mcu/details.tsx", {id: "MCUMovieDetail"},[
+    route("/mcu-index/detail/:movieId/:movieName", "routes/mcu/details.tsx", {id: "MCUMovieDetail"},[
         route("rate-movie/", "routes/mcu/rateMovie.tsx", {id: "MCUMovieVote"}),
     ],),
     route("/watchlist", "routes/mcu/watchlist.tsx", {id: "MCUWatchlist"}),
